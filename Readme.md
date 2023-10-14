3c5d57daae5e5d83151266949feda729477957ad
ArnoldKebaso <arnoldkebaso8@gmail.com>
2023-10-14 14:10:13 +0300

    My assignment

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This is my assignment for week 2 in Guru Academy
---
+++ main.js
@@ -0,0 +1,72 @@
+
+// ASSIGNMENT
+//Please follow insructions.
+//create the variables
+//print out your values.
+
+// 1. Create a variable firstname, lastname and email, given it the value of string.Your fristname, lastname and email address.
+//2. Create a variable of number given it the value of your age.
+//3. Create a variable iAmLearningJavaScript and give it a boolean value of true.
+
+//Advanced question, please do attempt them, make research and solve it.
+
+//4. Create a variable name called sentence and assign it a value of the concatenation of the other variables, making a sentence with it. 
+//hint: "My name is Kennedy Chukwu, I am 15 years old and it's true am learning Javascript".
+
+//HAPPY CODING...
+let firstName = "Arnold";
+let lastName = "Kebaso";
+let email = "arnold008@gmail.com";
+var age = 22;
+
+let iAmLearningJavaScript = "true";
+
+var sentence = `My name is ${firstName} ${lastName}, I am ${age} years old and it's ${iAmLearningJavaScript} am learning JavaScript `;
+
+console.log(firstName);
+console.log(lastName);
+console.log(email);
+console.log(age);
+console.log(sentence);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
